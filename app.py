ca84f5870d51ca4414e53a60beb68f7e2e49162e
Andrew Laing <andrew.laing.uk@gmail.com>
2024-04-16 15:20:45 +0100

    Modified app.py to show the AlbumRepository find method

--- app.py
+++ app.py
@@ -9,14 +9,6 @@ connection.connect()
 # Seed with some seed data
 connection.seed("seeds/music_library.sql")
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
-
-# List them out
-for artist in artists:
-    print(artist)
-
 # Retrieve all albums
 album_repository = AlbumRepository(connection)
 albums = album_repository.all()
@@ -24,3 +16,7 @@ albums = album_repository.all()
 # List them out
 for album in albums:
     print(album)
+
+# Retrieve the album with id 1
+album_1 = album_repository.find(1)
+print(f"\nThe album with id 1 is {album_1}")
